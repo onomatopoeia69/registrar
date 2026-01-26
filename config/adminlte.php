459995d0ac439186b8ae1793f5f716d05a5b1275
onomatopoeia69 <carljameslangres@gmail.com>
2026-01-26 19:21:27 +0800

    feat: sampe chart added , customized if needed

--- config/adminlte.php
+++ config/adminlte.php
@@ -562,7 +562,7 @@ return [
             ],
         ],
         'Chartjs' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
--- resources/views/pages/registrar/⚡home.blade.php
+++ resources/views/pages/registrar/⚡home.blade.php
@@ -33,32 +33,56 @@ class extends Component
             <div class="row">
                 
                 <div class="col-lg-4 col-6">
-                    <x-adminlte-small-box title="0" text="Total Users" 
+                    <x-adminlte-small-box title="0" text="Enrolled Students" 
                         icon="fas fa-users text-dark" theme="teal" 
                         url="#" url-text="View details"/>
                 </div>
 
                 <div class="col-lg-4 col-6">
-                    <x-adminlte-small-box title="0" text="Page Views" 
-                        icon="fas fa-eye text-dark" theme="info" 
+                    <x-adminlte-small-box title="0" text="New Enrollees" 
+                        icon="fas bi-people-fill text-dark" theme="info" 
                         url="#" url-text="View details"/>
                 </div>
 
                 <div class="col-lg-4 col-6">
-                    <x-adminlte-small-box title="0" text="Revenue" 
-                        icon="fas fa-shopping-cart text-dark" theme="success" 
+                    <x-adminlte-small-box title="0" text="Subjects Offered" 
+                        icon="fas bi-book-fill text-dark" theme="danger" 
+                        url="#" url-text="View details"/>
+                </div>
+                <div class="col-lg-4 col-6">
+                    <x-adminlte-small-box title="0" text="File Requests" 
+                        icon="fas bi-file-earmark-break-fill text-dark" theme="success" 
                         url="#" url-text="View details"/>
                 </div>
             </div>
             
+
+        </div>
+    </div>
+</section>
+
+
+<section id="box-info" class="container-fluid pt-4">
+    <div class="card shadow-sm">
+        <div class="card-header">
+            <h3 class="card-title text-bold fs-3">Charts Reports</h3>
+        </div>
+
+        <div class="card-body">
             <div class="row">
+
+                <div class="card-body">
+                    <canvas id="studentsChart" height="120" width="50%"></canvas>
                 </div>
+                
+            <div class="col-lg-4 col-6">
+            </div>
         </div>
     </div>
 </section>
 
     
-     
+
         @if(session('welcome'))
         <x-toast id="liveToast" color="primary">{{ session('welcome') }}  {{ Auth::user()->name }}</x-toast>
         @endif
@@ -72,6 +96,31 @@ class extends Component
         setTimeout(() => {
            $('#liveToast').toast('show');
         }, 5000);
+
+
+    const ctx = document.getElementById('studentsChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['BSIS', 'BSAIS', 'BSTM', 'BSCrim', 'BSHM'],
+            datasets: [{
+                label: 'Number of Subjects',
+                data: [40, 38, 45, 32, 28],
+                backgroundColor: '#9BD0F5',
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: true
+                }
+            }
+        }
+    });
+
 
 
 </script>
